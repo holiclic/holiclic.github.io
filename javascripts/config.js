6dfd9ecb84424c184a42481c23da891febc10b33
holicli <https://holicli@gitee.com>
2020-08-25 14:50:05 +0800

    1

---
+++ javascripts/config.js
@@ -0,0 +1,18 @@
+var disqus_shortname = 'holicli';			//Short Name of disqus, this parameter is for disqus comment box.
+var hostbase = 'http://holicli.github.io';		//The absolute root address of your blog without '/' in the end.
+var githubname = 'holicli';						//Your username of GitHub.
+var repos = 'holicli.github.io';					//Name of repository that contains your blog.
+var sitetitle = 'holicli';						//Name of your blog.
+var rss = '';									//Rss address of your blog.
+var suffix = '';								//Suffix of post files, '.' should be included.
+var branch = '';								//Branch of your blog repository. 'master' is default if you left it blank.
+
+//Sample Config:
+//var disqus_shortname = 'sneezry';
+//var hostbase = 'http://sneezry.com/blog';
+//var githubname = 'sneezry';
+//var repos = 'blog';	
+//var sitetitle = 'Sneezry';
+//var rss = '';
+//var suffix = '.md';
+//var branch = 'gh-pages';
