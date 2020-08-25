1ba016b1a54c8de24c8c935be269c674a83ce316
holiclic <70193773+holiclic@users.noreply.github.com>
2020-08-25 14:51:58 +0800

    Update config.js

--- javascripts/config.js
+++ javascripts/config.js
@@ -1,8 +1,8 @@
-var disqus_shortname = 'holicli';			//Short Name of disqus, this parameter is for disqus comment box.
-var hostbase = 'http://holicli.github.io';		//The absolute root address of your blog without '/' in the end.
-var githubname = 'holicli';						//Your username of GitHub.
-var repos = 'holicli.github.io';					//Name of repository that contains your blog.
-var sitetitle = 'holicli';						//Name of your blog.
+var disqus_shortname = 'holiclic';			//Short Name of disqus, this parameter is for disqus comment box.
+var hostbase = 'http://holiclic.github.io';		//The absolute root address of your blog without '/' in the end.
+var githubname = 'holiclic';						//Your username of GitHub.
+var repos = 'holiclic.github.io';					//Name of repository that contains your blog.
+var sitetitle = 'holiclic';						//Name of your blog.
 var rss = '';									//Rss address of your blog.
 var suffix = '';								//Suffix of post files, '.' should be included.
 var branch = '';								//Branch of your blog repository. 'master' is default if you left it blank.
